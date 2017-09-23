d59a244232c8ac1aeacdb0011baa5caed5c060b0
Sandra Velez <sandyvelez@gmail.com>
2017-09-23 17:47:49 -0400

    trying stuff with table colors

--- designs.js
+++ designs.js
@@ -2,38 +2,43 @@ let selectedColor;
 let selectedWidth, selectedHeight;
 
 // Select color input
-$('#colorPicker').on('input', function(){
-    selectedColor = $('#colorPicker').val();
+$('#colorPicker').on('input', function() {
+  selectedColor = $('#colorPicker').val();
 });
 
 // Select size input
-$('#input_height').on('input', function(){
-    selectedHeight = $('#input_height').val();
+$('#input_height').on('input', function() {
+  selectedHeight = $('#input_height').val();
 });
 
-$('#input_width').on('input', function(){
-    selectedWidth = $('#input_width').val();
+$('#input_width').on('input', function() {
+  selectedWidth = $('#input_width').val();
 });
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').on('submit', function(){
-    event.preventDefault();
-    makeGrid();
+$('#sizePicker').on('submit', function() {
+  event.preventDefault();
+  makeGrid();
 });
 
 function makeGrid() {
-  let grid = $('#pixel_canvas');
-  let tbl = $('<tr><td>First name</td><td>Last name</td></tr>');
 
-  for (let r = 0; r < selectedHeight; r++)
-  {
+  let grid = $('#pixel_canvas');
+
+  // clear prior table, if any
+  contents = grid.find('*');
+  contents.remove();
+
+  for (let r = 0; r < selectedHeight; r++) {
     let row = $('<tr></tr>');
     grid.append(row);
 
-    for (let c = 0; c < selectedWidth; c++)
-    {
+    for (let c = 0; c < selectedWidth; c++) {
       let column = $('<td></td>');
       row.append(column);
     }
   }
+
+    // $('tr,td').css("border-color", selectedColor);
+    $('td').css("background-color", selectedColor);
 }
