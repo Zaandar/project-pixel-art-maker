3d079568402f7fe1d3dd0f14e40b3807f2c2b5ea
Sandra Velez <sandyvelez@gmail.com>
2017-09-23 16:19:26 -0400

    Initial makeGrid

--- designs.js
+++ designs.js
@@ -4,28 +4,36 @@ let selectedWidth, selectedHeight;
 // Select color input
 $('#colorPicker').on('input', function(){
     selectedColor = $('#colorPicker').val();
-    console.log(selectedColor);
 });
 
 // Select size input
 $('#input_height').on('input', function(){
     selectedHeight = $('#input_height').val();
-    console.log(selectedHeight);
 });
 
 $('#input_width').on('input', function(){
     selectedWidth = $('#input_width').val();
-    console.log(selectedWidth);
 });
 
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').on('submit', function(){
-    console.log("submitted");
+    event.preventDefault();
     makeGrid();
 });
 
 function makeGrid() {
+  let grid = $('#pixel_canvas');
+  let tbl = $('<tr><td>First name</td><td>Last name</td></tr>');
 
-console.log("GRID!!!");
+  for (let r = 0; r < selectedHeight; r++)
+  {
+    let row = $('<tr></tr>');
+    grid.append(row);
 
+    for (let c = 0; c < selectedWidth; c++)
+    {
+      let column = $('<td></td>');
+      row.append(column);
+    }
+  }
 }
